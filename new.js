89f18d16912645ce467be92e89ae8c6e6bb88f56
Max <136755267+maax36@users.noreply.github.com>
2025-03-20 15:30:04 +0300

    Update new.js
    fix

--- new.js
+++ new.js
@@ -27,47 +27,29 @@ dBody.appendChild(alButton);
 function notifications() {
 	checkElements = setInterval(() => {
 	
-	document.getElementById('reloadList').click();
-		
+	const resultBlock = document.querySelector("#resultBlock");
     const parentElement = document.querySelector("#totalStatus");
-    const resultBlock = document.querySelector("#resultBlock");
+    
+    if (parentElement && resultBlock.innerHTML === "") {
+    	document.getElementById('reloadList').click();
+  		const asd = document.querySelector(".totalPrintList > .in b");
+      console.log(asd.textContent);
 
-
-    if (parentElement) {
-
-        		const asd = document.querySelector(".totalPrintList > .in b");
-		        console.log(asd.textContent);
-        
-								
-								if(resultBlock.innerHTML === "") {
-									
-									
-									switch(true) {
-									  case (asd.textContent >= 5):
-									    console.log("Больше 5 заказов!");
-								  		var audio = new Audio('https://btones.b-cdn.net/fetch/73/7380ac19fc79ea4705ff6d5d5b16d8ad.mp3');
-								  		audio.play();
-									    break;
-									
-									  case (asd.textContent >= 10):
-									    console.log("Больше 10 заказов!");
-								  		var audio = new Audio('https://btones.b-cdn.net/fetch/73/7380ac19fc79ea4705ff6d5d5b16d8ad.mp3');
-								  		audio.play();
-									    break;
-									
-									  default:
-									    console.log("Меньше 5 заказов!");
-									    break;
-									}
-									
-								  
-								}
-								else {
-									console.log("не выводим уведомление, страница заказа1");
-								}
-
-      
-        //clearInterval(checkElements);
+				if (asd.textContent >= 10) {
+          console.log("Больше 10 заказов!");
+              var audio = new Audio('https://btones.b-cdn.net/fetch/02/02a7bc8c845ecccfd014776de3010b31.mp3');
+              audio.play();
+          } else if (asd.textContent >= 5) {
+              console.log("Больше 5 заказов!");
+              var audio = new Audio('https://btones.b-cdn.net/fetch/73/7380ac19fc79ea4705ff6d5d5b16d8ad.mp3');
+              audio.play();
+          } else {
+              console.log("Меньше 5 заказов!");
+          }
+			 
+			
+    } else {
+    	console.log("Страница заказа");
     }
-}, 2000);
+}, 180000);
 }
